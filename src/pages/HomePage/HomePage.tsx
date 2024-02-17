1df0fa5a062323542a5800b9807c5cb98b69bb30
Aroop Biswal <aroopbiswal@gmail.com>
2024-02-16 18:57:00 -0800

    changes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -28,13 +28,23 @@ const HomePage: React.FC = () => {
                 --gradient-color-2: #3a3aff;
                 --gradient-color-3: #ff61ab;
                 --gradient-color-4: #E63946;
-                width: 100vw;
-                height: 100vh;
               }
+
+              .rotate-div {
+                transform: rotate(-15deg); /* Rotate the div by -30 degrees */
+                display: inline-block; /* Ensures the div takes only the necessary width */
+                transform-origin: bottom left; /* Rotate around the top-left corner */
+                width: 125vw;
+            }
             `}
           </style>
           <body>
-            <canvas id="gradient-canvas"></canvas>
+            <div className="relative">
+
+            <canvas className="absolute inset-0 transform rounded-bl-3xl rounded-br-3xl rotate-{30} transform-gpu" id="gradient-canvas"></canvas>
+            <div className="absolute .rotate-div inset-0 bg-white "></div>
+              hi
+            </div>
           </body>
         </html>
       );
