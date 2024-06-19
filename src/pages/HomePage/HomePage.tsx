ff70ce8f569a2db8941d0c0aae9c58459167461b
Aroop Biswal <aroopbiswal@gmail.com>
2024-06-18 21:33:45 -0700

    updates

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,53 +1,19 @@
 import React, { useEffect } from 'react';
-import Gradient from './Gradient.js'; 
+import CoverCard from '../../components/coverCard/coverCard';   
 
-const HomePage: React.FC = () => {
-    useEffect(() => {
-        const gradient = new Gradient();
-        gradient.initGradient('#gradient-canvas');
-    
-        // Cleanup: remove the gradient instance when the component unmounts
-        return () => {
-        };
-      }, []);
-    
-      return (
-        <html>
-          <head>
-            <title>Aroop Biswal</title>
-          </head>
-          <style>
-            {`
-              body {
-                margin: 0;
-                padding: 0;
-              }
-    
-              #gradient-canvas {
-                --gradient-color-1: #6ec3f4;
-                --gradient-color-2: #3a3aff;
-                --gradient-color-3: #ff61ab;
-                --gradient-color-4: #E63946;
-              }
-
-              .rotate-div {
-                transform: rotate(-15deg); /* Rotate the div by -30 degrees */
-                display: inline-block; /* Ensures the div takes only the necessary width */
-                transform-origin: bottom left; /* Rotate around the top-left corner */
-                width: 125vw;
-            }
-            `}
-          </style>
-          <body>
-            <div className="relative">
-
-            <canvas className="absolute inset-0 transform rounded-bl-3xl rounded-br-3xl rotate-{30} transform-gpu" id="gradient-canvas"></canvas>
-            <div className="absolute .rotate-div inset-0 bg-white "></div>
-              hi
-            </div>
-          </body>
-        </html>
-      );
-    };
+export const HomePage: React.FC = () => {
+  return (
+    <CoverCard
+    title="African<br />forest"
+    image="/background.jpg"
+    subTitle="African forest tour"
+    text={`An amazing history, stunning nature, and a high level of
+    development make the country unique on the African continent.
+    Two oceans, savannah, mountains, desert, rainforest and the
+    rythms of African drums.`}
+    link="#"
+  />
+  );
+}
 
 export default HomePage;
